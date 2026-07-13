3aec051e950e01afddd306604121f432bb1f45c3
MilesWK <mileswkrueger@gmail.com>
2026-07-13 12:26:40 -0400

    Add animation for bubbles

--- public/script.js
+++ public/script.js
@@ -1,30 +1,42 @@
+const message_container = document.getElementById("messages")
+const prompt_input = document.getElementById("chat")
 // These two functions add messages to the HTML DOM
-
 function addResponseMessage(msg) {
     const message = document.createElement("div")
-    const message_container = document.getElementById("messages")
     message.className = "ai chatbox"
     message.textContent = msg
     message_container.appendChild(message)
+
+    gsap.from(message, {scale: 0.3,  duration: 1, ease: "elastic.out(1.2, 0.3)"});
+
 }
 
 function addUserMessage(msg) {
     const message = document.createElement("div")
     const message_precontainer = document.createElement("div") // This is that container that goes around it.
-    const message_container = document.getElementById("messages") // this is where all the messages go 
     message.className = "user chatbox"
     message.textContent = msg
     message_precontainer.className = "chatbox-container"
     // Don't think about this too hard.
     message_precontainer.appendChild(message)
     message_container.appendChild(message_precontainer)
+
+    gsap.from(message, {scale: 0.3,  duration: 1, ease: "elastic.out(1.2, 0.3)"});
 }
 
-messages = [
-    {"user": "hi"},
-    {"ai": "hi"},
-    {"user": "This is a demo message."}
-]
 
-addUserMessage("This is added using JS")
-addResponseMessage("This is added using JS")
+// addUserMessage("A message here.")
+//addResponseMessage("The response goes here!")
+// addUserMessage("Are you AI?")
+// addResponseMessage("Maybe I am...")
+// addResponseMessage("...maybe not")
+// addUserMessage("👀👀👀")
+
+prompt_input.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        if (prompt_input.value) {
+            addUserMessage(prompt_input.value)
+            prompt_input.value = ""
+        }
+    }
+});
